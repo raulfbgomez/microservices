2aa4fd62a0a9a18f55ea68287488bcf84f4ff1ce
Raul Gomez <raul_nouni@hotmail.com>
2019-12-28 22:53:44 -0600

    Avanzando en classifieds app

---
+++ classifieds-app/src/components/Root/Root.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  box-sizing: border-box;
+  height: 100%;
+  padding: 1rem;
+  width: 100%;
+`
+const Container = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 0 auto;
+  width: 80rem;
+`
+const Content = styled.div`
+  flex: 1;
+  margin-right: 1rem;
+`
+
+const Sidebar = styled.div`
+  flex: 0;
+  width: 10rem;
+`
+
+const Root = () => {
+  return <Wrapper>
+    <Container>
+      <Content>asdfdsa</Content>
+      <Sidebar>fdsafsad</Sidebar>
+    </Container>
+  </Wrapper>
+}
+
+export default Root
