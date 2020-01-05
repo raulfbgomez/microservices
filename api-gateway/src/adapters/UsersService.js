e792ebf4e61773f9a9fa7fc763cf3f51757f630a
Raul Gomez <raul_nouni@hotmail.com>
2020-01-04 22:41:04 -0600

    Creando API para crear sesiones y gateway con GraphQL

--- api-gateway/src/adapters/UsersService.js
+++ api-gateway/src/adapters/UsersService.js
@@ -11,4 +11,9 @@ export default class UsersService {
     }).json()
     return body
   }
+
+  static async createUserSession({ email, password }) {
+    const body = await got.post(`${USERS_SERVICE_URI}/sessions`, { json: {email, password }}).json()
+    return body
+  }
 }
